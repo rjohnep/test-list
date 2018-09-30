89e6c548de9a4883907eccf8604866c2069d8b99
Eugene Rogach <eugene.rogach@pandadoc.com>
2018-09-30 16:29:21 +0300

    gh-pages configuring hash history

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { ConnectedRouter } from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/createHashHistory';
 
 import 'normalize.css';
 import 'typeface-pt-sans';
